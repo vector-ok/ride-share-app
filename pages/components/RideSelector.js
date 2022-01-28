9f4f578db1bae410a26a9c345a673ba34681bf23
ExciteNG <kehinde.ibraheem@exciteafrica.com>
2022-01-28 02:26:53 +0100

    move car data file

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import tw from 'tailwind-styled-components';
-import { carList } from '../data/carList';
+import { carList } from '../../data/carList';
 
 function RideSelector({ pickupCoordinates, dropoffCoordinates }) {
   const [rideDuration, setRideDuration] = useState(0);
